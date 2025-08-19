7e84c0b3f6f99b11fb2eac1eb4d4e81e1a19eda7
Rubèn <numbworks@gmail.com>
2025-08-19 15:48:59 +0200

    #1 *tests: Iteration 4.

--- src/nwmakefilertests.sh
+++ src/nwmakefilertests.sh
@@ -15,6 +15,20 @@ assert_failure() {
         echo "Passed: $*"
     fi
 }
+assert_in_list() {
+    local expected="$1"
+    shift
+    local list=("$@")
+
+    for item in "${list[@]}"; do
+        if [[ "$item" == "$expected" ]]; then
+            echo "Passed"
+            return
+        fi
+    done
+
+    echo "Failed"
+}
 
 # TESTS
 test_validatemoduleversion_shouldreturnexpectedexitcode_whenvalidgument() {
@@ -47,6 +61,45 @@ test_validatecoveragethreshold_shouldreturnexpectedexitcode_wheninvalidgument() 
     assert_failure validate_coverage_threshold "abc"
     assert_failure validate_coverage_threshold ""
 }
+test_addtofunctionnamess1_shouldcontainexpecteditem_wheninvoked() {
+    echo "${FUNCNAME[0]}"
+
+    # Arrange
+    function_names_s1=()
+    function_name="some_function_name"
+
+    # Act
+    add_to_function_names_s1 $function_name
+
+    # Assert
+    assert_in_list $function_name "${function_names_s1[@]}"
+}
+test_addtofunctionnamess2_shouldcontainexpecteditem_wheninvoked() {
+    echo "${FUNCNAME[0]}"
+
+    # Arrange
+    function_names_s2=()
+    function_name="some_function_name"
+
+    # Act
+    add_to_function_names_s2 $function_name
+
+    # Assert
+    assert_in_list $function_name "${function_names_s2[@]}"
+}
+test_addtofunctionnamess3_shouldcontainexpecteditem_wheninvoked() {
+    echo "${FUNCNAME[0]}"
+
+    # Arrange
+    function_names_s3=()
+    function_name="some_function_name"
+
+    # Act
+    add_to_function_names_s3 $function_name
+
+    # Assert
+    assert_in_list $function_name "${function_names_s3[@]}"
+}
 
 # TEST NAMES
 declare -a test_names=(
@@ -54,6 +107,9 @@ declare -a test_names=(
     "test_validatemoduleversion_shouldreturnexpectedexitcode_wheninvalidgument"
     "test_validatecoveragethreshold_shouldreturnexpectedexitcode_whenvalidgument"
     "test_validatecoveragethreshold_shouldreturnexpectedexitcode_wheninvalidgument"
+    "test_addtofunctionnamess1_shouldcontainexpecteditem_wheninvoked"
+    "test_addtofunctionnamess2_shouldcontainexpecteditem_wheninvoked"
+    "test_addtofunctionnamess3_shouldcontainexpecteditem_wheninvoked"
 )
 
 # MAIN
