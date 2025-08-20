fb5ff64d6fa5da7e705fc849544f01396ec0b686
Rubèn <numbworks@gmail.com>
2025-08-20 21:15:38 +0200

    #1 handle_save(): finetuning - Iteration 4.

--- src/nwmakefiler.sh
+++ src/nwmakefiler.sh
@@ -39,17 +39,17 @@ makefile-info:
 EOF
 }
 create_section2_changelog_concise() {
-    cat <<EOF
+    cat <<'EOF'
 changelog-concise:
-	@value=\$$(cat \$(ROOT_DIR)/CHANGELOG | grep -c -e "v\$(MODULE_VERSION)\$\$" -e "v\$(MODULE_VERSION) - BREAKING CHANGES\$\$"); \\
-	if [ \$\$value -eq 1 ]; then echo "[OK] \$@: 'CHANGELOG' updated to current version!"; else echo "[WARNING] \$@: 'CHANGELOG' not updated to current version!"; fi;
+    @value=\$\$(cat \$\$(ROOT_DIR)/CHANGELOG | grep -c -e "v\$\$(MODULE_VERSION)\$\$" -e "v\$\$(MODULE_VERSION) - BREAKING CHANGES\$\$"); \\
+    if [ \$\$value -eq 1 ]; then echo "[OK] \$@: 'CHANGELOG' updated to current version!"; else echo "[WARNING] \$@: 'CHANGELOG' not updated to current version!"; fi;
 EOF
 }
 create_section2_codemetrics_concise() {
-    cat <<EOF
+    cat <<'EOF'
 codemetrics-concise:
-	@value=\$$(radon cc -a -s \$(ROOT_DIR)/src/\$(MODULE_NAME)*.py  | grep -oP "Average complexity: \\K[A-F]"); \\
-	if [[ "\$\$value" == *"A"* ]]; then echo "[OK] \$@: the cyclomatic complexity is excellent ('\$\$value')."; else echo "[WARNING] \$@: the cyclomatic complexity is not excellent ('\$\$value')"; fi;
+    @value=\$\$(radon cc -a -s \$\$(ROOT_DIR)/src/\$\$(MODULE_NAME)*.py | grep -oP "Average complexity: \\K[A-F]"); \\
+    if [[ "\$\$value" == *"A"* ]]; then echo "[OK] \$@: the cyclomatic complexity is excellent ('\$\$value')."; else echo "[WARNING] \$@: the cyclomatic complexity is not excellent ('\$\$value')"; fi;
 EOF
 }
 create_section2_codemetrics_verbose() {
@@ -60,11 +60,11 @@ codemetrics-verbose:
 EOF
 }
 create_section2_compile_concise() {
-    cat <<EOF
+    cat <<'EOF'
 compile-concise:
-	@value=\$$(python -m py_compile \$(ROOT_DIR)/src/\$(MODULE_NAME).py 2>&1); \\
-	if [ -z "\$\${value}" ]; then value=0; else value=1; fi; \\
-	if [ \$\$value -eq 0 ]; then echo "[OK] \$@: compiling the library throws no issues."; else echo "[WARNING] \$@: compiling the library throws some issues."; fi;
+    @value=\$\$(python -m py_compile \$\$(ROOT_DIR)/src/\$\$(MODULE_NAME).py 2>&1); \\
+    if [ -z "\$\${value}" ]; then value=0; else value=1; fi; \\
+    if [ \$\$value -eq 0 ]; then echo "[OK] \$@: compiling the library throws no issues."; else echo "[WARNING] \$@: compiling the library throws some issues."; fi;
 EOF
 }
 create_section2_compile_verbose() {
@@ -75,12 +75,12 @@ compile-verbose:
 EOF
 }
 create_section2_coverage_concise() {
-    cat <<EOF
+    cat <<'EOF'
 coverage-concise:
-	@cd \$(ROOT_DIR)/tests/; \\
-	coverage run -m unittest \$(MODULE_NAME)tests.py > /dev/null 2>&1; \\
-	value=\$$(coverage report --include=\$(MODULE_NAME).py | grep -oP 'TOTAL\\s+\\d+\\s+\\d+\\s+\\K\\d+(?=%)'); \\
-	if [ \$\$value -ge \$(COVERAGE_THRESHOLD) ]; then echo "[OK] \$@: unit test coverage >= \$(COVERAGE_THRESHOLD)%."; else echo "[WARNING] \$@: unit test coverage < \$(COVERAGE_THRESHOLD)%."; fi;
+    @cd \$\$(ROOT_DIR)/tests/; \\
+    coverage run -m unittest \$\$(MODULE_NAME)tests.py > /dev/null 2>&1; \\
+    value=\$\$(coverage report --include=\$\$(MODULE_NAME).py | grep -oP 'TOTAL\\s+\\d+\\s+\\d+\\s+\\K\\d+(?=%)'); \\
+    if [ \$\$value -ge \$\$(COVERAGE_THRESHOLD) ]; then echo "[OK] \$@: unit test coverage >= \$\$(COVERAGE_THRESHOLD)%."; else echo "[WARNING] \$@: unit test coverage < \$\$(COVERAGE_THRESHOLD)%."; fi;
 EOF
 }
 create_section2_coverage_verbose() {
@@ -96,11 +96,11 @@ coverage-verbose:
 EOF
 }
 create_section2_docstrings_concise() {
-    cat <<EOF
+    cat <<'EOF'
 docstrings-concise:
-	@file_path=\$(ROOT_DIR)/src/\$(MODULE_NAME).py; \\
-	value=\$$(python -m nwdocstringchecking -fp \$\$file_path -e MessageCollection -e init -e str -e repr); \\
-	if [[ "\$\$value" == *"All methods have docstrings."* ]]; then echo "[OK] \$@: all methods have docstrings."; else echo "[WARNING] \$@: not all methods have docstrings."; fi;
+    @file_path=\$\$(ROOT_DIR)/src/\$\$(MODULE_NAME).py; \\
+    value=\$\$(python -m nwdocstringchecking -fp \$\$file_path -e MessageCollection -e init -e str -e repr); \\
+    if [[ "\$\$value" == *"All methods have docstrings."* ]]; then echo "[OK] \$@: all methods have docstrings."; else echo "[WARNING] \$@: not all methods have docstrings."; fi;
 EOF
 }
 create_section2_docstrings_verbose() {
@@ -112,10 +112,10 @@ docstrings-verbose:
 EOF
 }
 create_section2_setup_concise() {
-    cat <<EOF
+    cat <<'EOF'
 setup-concise:
-	@value=\$$(cat \$(ROOT_DIR)/src/setup.py | grep -oP 'MODULE_VERSION\\s*:\\s*str\\s*=\\s*"\K[\\d.]+'); \\
-	if [ \$\$value == "\$(MODULE_VERSION)" ]; then echo "[OK] \$@: 'setup.py' updated to current version!"; else echo "[WARNING] \$@: 'setup.py' not updated to current version!"; fi;
+    @value=\$\$(cat \$\$(ROOT_DIR)/src/setup.py | grep -oP 'MODULE_VERSION\\s*:\\s*str\\s*=\\s*"\K[\\d.]+'); \\
+    if [ \$\$value == "\$\$(MODULE_VERSION)" ]; then echo "[OK] \$@: 'setup.py' updated to current version!"; else echo "[WARNING] \$@: 'setup.py' not updated to current version!"; fi;
 EOF
 }
 create_section2_tryinstall_concise() {
@@ -148,11 +148,11 @@ tryinstall-verbose:
 EOF
 }
 create_section2_type_concise() {
-    cat <<EOF
+    cat <<'EOF'
 type-concise:
-	@value=\$$(mypy \$(ROOT_DIR)/src/\$(MODULE_NAME).py --disable-error-code=import-untyped | grep -c "error:"); \\
-	value+=\$$(mypy \$(ROOT_DIR)/tests/\$(MODULE_NAME)tests.py --disable-error-code=import-untyped  --disable-error-code=import-not-found | grep -c "error:"); \\
-	if [ \$\$value -eq 0 ]; then echo "[OK] \$@: passed!"; else echo "[WARNING] \$@: not passed! '\$\$value' error(s) found!"; fi;
+    @value=\$\$(mypy \$\$(ROOT_DIR)/src/\$\$(MODULE_NAME).py --disable-error-code=import-untyped | grep -c "error:"); \\
+    value+=\$\$(mypy \$\$(ROOT_DIR)/tests/\$\$(MODULE_NAME)tests.py --disable-error-code=import-untyped --disable-error-code=import-not-found | grep -c "error:"); \\
+    if [ \$\$value -eq 0 ]; then echo "[OK] \$@: passed!"; else echo "[WARNING] \$@: not passed! '\$\$value' error(s) found!"; fi;
 EOF
 }
 create_section2_type_verbose() {
@@ -164,11 +164,11 @@ type-verbose:
 EOF
 }
 create_section2_unittest_concise() {
-    cat <<EOF
+    cat <<'EOF'
 unittest-concise:
-	@value=\$$(python \$(ROOT_DIR)/tests/\$(MODULE_NAME)tests.py 2>&1 | grep -oP '(?<=Ran )\\d+(?= tests)'); \\
-	if [ -z "\$\${value}" ]; then value=0; fi; \\
-	if [ \$\$value -gt 0 ]; then echo "[OK] \$@: '\$\$value' tests found and run."; else echo "[WARNING] \$@: '\$\$value' tests found and run."; fi;
+    @value=\$\$(python \$\$(ROOT_DIR)/tests/\$\$(MODULE_NAME)tests.py 2>&1 | grep -oP '(?<=Ran )\\d+(?= tests)'); \\
+    if [ -z "\$\${value}" ]; then value=0; fi; \\
+    if [ \$\$value -gt 0 ]; then echo "[OK] \$@: '\$\$value' tests found and run."; else echo "[WARNING] \$@: '\$\$value' tests found and run."; fi;
 EOF
 }
 create_section2_unittest_verbose() {
@@ -191,19 +191,19 @@ calculate-commitavg:
 EOF
 }
 create_section3_create_classdiagram() {
-    cat <<EOF
+    cat <<'EOF'
 create-classdiagram:
-	@clear; \\
-	pyreverse \$(ROOT_DIR)/src/\$(MODULE_NAME).py -o mmd -d /home/\$(MODULE_NAME)/; \\
-	md_file="/home/\$(MODULE_NAME)/classes.mmd"; \\
-	tmp_file="/home/\$(MODULE_NAME)/temp.mmd"; \\
-	final_file="/home/\$(MODULE_NAME)/Diagram-Architecture.md"; \\
-	awk '/classDiagram|--\\*/ { print }' \$\$md_file > \$\$tmp_file; \\
-	echo '```mermaid' > \$\$md_file; \\
-	cat \$\$tmp_file >> \$\$md_file; \\
-	echo '```' >> \$\$md_file; \\
-	rm \$\$tmp_file; \\
-	mv \$\$md_file \$\$final_file
+    @clear; \\
+    pyreverse \$\$(ROOT_DIR)/src/\$\$(MODULE_NAME).py -o mmd -d /home/\$\$(MODULE_NAME)/; \\
+    md_file="/home/\$\$(MODULE_NAME)/classes.mmd"; \\
+    tmp_file="/home/\$\$(MODULE_NAME)/temp.mmd"; \\
+    final_file="/home/\$\$(MODULE_NAME)/Diagram-Architecture.md"; \\
+    awk '/classDiagram|--\\*/ { print }' \$\$md_file > \$\$tmp_file; \\
+    echo '```mermaid' > \$\$md_file; \\
+    cat \$\$tmp_file >> \$\$md_file; \\
+    echo '```' >> \$\$md_file; \\
+    rm \$\$tmp_file; \\
+    mv \$\$md_file \$\$final_file
 EOF
 }
 create_section3_check_pythonversion() {
@@ -224,17 +224,17 @@ check-requirements:
 EOF
 }
 create_section3_update_codecoverage() {
-    cat <<EOF
+    cat <<'EOF'
 update-codecoverage:
-	@clear; \\
-	cd \$(ROOT_DIR)/tests/; \\
-	coverage run -m unittest \$(MODULE_NAME)tests.py > /dev/null 2>&1; \\
-	value=\$$(coverage report --include=\$(MODULE_NAME).py | grep -oP 'TOTAL\\s+\\d+\\s+\\d+\\s+\\K\\d+(?=%)'); \\
-	if [ \$\$value -le 39 ]; then color="red"; elif [ \$\$value -le 69 ]; then color="orange"; else color="green"; fi; \\
-	url="https://img.shields.io/badge/coverage-\$\${value}.0%25-\$\${color}"; \\
-	echo "\$\$url" > \$(ROOT_DIR)/codecoverage.txt; \\
-	curl -s \$\$url -o \$(ROOT_DIR)/codecoverage.svg; \\
-	if [ \$\$? -eq 0 ]; then echo "[OK] \$@: coverage badge updated successfully!"; else echo "[ERROR] \$@: failed to update coverage badge."; fi;
+    @clear; \\
+    cd \$\$(ROOT_DIR)/tests/; \\
+    coverage run -m unittest \$\$(MODULE_NAME)tests.py > /dev/null 2>&1; \\
+    value=\$\$(coverage report --include=\$\$(MODULE_NAME).py | grep -oP 'TOTAL\\s+\\d+\\s+\\d+\\s+\\K\\d+(?=%)'); \\
+    if [ \$\$value -le 39 ]; then color="red"; elif [ \$\$value -le 69 ]; then color="orange"; else color="green"; fi; \\
+    url="https://img.shields.io/badge/coverage-\$\${value}.0%25-\$\${color}"; \\
+    echo "\$\$url" > \$\$(ROOT_DIR)/codecoverage.txt; \\
+    curl -s \$\$url -o \$\$(ROOT_DIR)/codecoverage.svg; \\
+    if [ \$\$? -eq 0 ]; then echo "[OK] \$@: coverage badge updated successfully!"; else echo "[ERROR] \$@: failed to update coverage badge."; fi;
 EOF
 }
 
@@ -319,7 +319,7 @@ eval_function_names() {
 
     local content=""
     for function_name in "${fn_array[@]}"; do
-        output=$(eval "$function_name")
+        output=$($function_name)
         content+="$output"$'\n'
     done
 
